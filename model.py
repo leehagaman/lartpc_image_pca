9c18cdb9b5346be753b2e23af07c89afe04cdbdf
Lee Hagaman <hagamanlee@gmail.com>
2024-10-14 22:15:31 -0500

    added more data from multiple input files

--- model.py
+++ model.py
@@ -9,12 +9,13 @@ import time
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Using device: {device}")
 
-batch_size = 10
-num_epochs = 100
+batch_size = 100
+num_epochs = 1000
 lr = 0.001
 
-y_train = torch.tensor(np.load("processed_data_files/nue_images.npz")["image_stack"]) / 100.
+y_train = torch.tensor(np.load("processed_data_files/nue_images_combined.npz")["image_stack"]) / 100.
 num_train = y_train.shape[0]
+print(f"loaded {num_train} training images")
 x_train = torch.tensor(np.arange(num_train))
 
 # splitting into batches, not using the last batch which will have a different batch size
@@ -71,33 +72,32 @@ class FeaturesToImage(nn.Module):
         x = self.sigmoid(self.deconv7(x)) # (batch_size, 1, 128, 128)
         return x
 
+# modified MSE loss, to get the model to pay more attention to non-zero pixels
 class WeightedMSELoss(nn.Module):
     def __init__(self):
         super(WeightedMSELoss, self).__init__()
-
     def forward(self, predicted, target):
-        # Standard MSE loss (squared difference)
-        loss = (predicted - target) ** 2
-        
-        # Weight the loss by the target values (higher weight for non-zero target pixels)
-        weighted_loss = loss * (target + 0.001)
-        
-        # Normalize the loss by the sum of the weights (avoid division by zero)
-        norm_factor = target.sum() + 1e-8  # Add small value to avoid division by zero
-        weighted_loss = weighted_loss.sum() / norm_factor
-        
-        return weighted_loss
+        loss = (predicted - target) ** 2 # normal MSE loss
+        weighted_loss = loss * (target + 0.001) # weighting up non-zero pixels
+        normalized_weighted_loss = weighted_loss.sum() / (target.sum() + 1e-8) # making it so images with more activity don't have higher loss
+        return normalized_weighted_loss
+
 
 model = FeaturesToImage().to(device)
-print(f"Number of model parameters: {sum(p.numel() for p in model.parameters())}")
 
-#criterion = nn.MSELoss()
+num_embedding_params = sum(p.numel() for p in model.embedding.parameters())
+num_total_params = sum(p.numel() for p in model.parameters())
+num_inference_params = num_total_params - num_embedding_params
+
+print(f"Number of embedding parameters (training only): {num_embedding_params}")
+print(f"Number of inference parameters: {num_inference_params}")
+
 criterion = WeightedMSELoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 
 os.system("rm -rf training_progress_images/*.png")
 
-epoch_print_interval = 10
+epoch_print_interval = 20
 
 # training loop
 print("Starting training loop...")
@@ -113,16 +113,17 @@ for epoch in range(num_epochs):
         optimizer.step()
     epoch_end_time = time.time()
     epoch_time = epoch_end_time - epoch_start_time
-    print(f"Epoch {epoch} of {num_epochs}, Loss: {loss.item()}, Time: {epoch_time:.2f} seconds")
-    if epoch % epoch_print_interval == 0:
-        print(f"    Saving truth and generated image side by side...")
-        plt.figure()
-        plt.subplot(1, 2, 1)
-        plt.imshow(y_train_batch.detach().cpu().numpy()[0,:,:], cmap="jet")
-        plt.colorbar()
-        plt.subplot(1, 2, 2)
-        plt.imshow(output.detach().cpu().numpy()[0,:,:], cmap="jet")
-        plt.colorbar()
-        plt.savefig(f"training_progress_images/epoch_{epoch}.png")
+    if epoch % epoch_print_interval == 0 or epoch == num_epochs - 1:
+        print(f"Epoch {epoch} of {num_epochs}, Loss: {loss.item()}, Time: {epoch_time:.2f} seconds")
+        print(f"    Saving truth and generated image side by side for three events...")
+        fig, axs = plt.subplots(2, 5, figsize=(10, 5))
+        for event_i in range(5):
+            axs[0, event_i].imshow(y_train_batch.detach().cpu().numpy()[event_i,:,:], cmap="jet", vmin=0, vmax=1)
+            axs[1, event_i].imshow(output.detach().cpu().numpy()[event_i,:,:], cmap="jet", vmin=0, vmax=1)
+        for ax in axs.flatten():
+            ax.axis("off")
+        plt.tight_layout()
+        plt.savefig(f"training_progress_images/epoch_{epoch:04d}.png")
         plt.close()
 
+print("Training done!")
