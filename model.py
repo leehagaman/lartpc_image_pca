dc6dcd3ff8dbf437c0e384cd329e429d036a988f
Lee Hagaman <hagamanlee@gmail.com>
2024-10-14 22:26:27 -0500

    added batchnorm and gelu

--- model.py
+++ model.py
@@ -34,6 +34,13 @@ class FeaturesToImage(nn.Module):
         super(FeaturesToImage, self).__init__()
         
         self.embedding = nn.Embedding(num_train, num_features)
+
+        self.bn1 = nn.BatchNorm2d(512)
+        self.bn2 = nn.BatchNorm2d(256)
+        self.bn3 = nn.BatchNorm2d(128)
+        self.bn4 = nn.BatchNorm2d(64)
+        self.bn5 = nn.BatchNorm2d(32)
+        self.bn6 = nn.BatchNorm2d(16)
     
         self.deconv1 = nn.ConvTranspose2d(num_features, 512, kernel_size=4, stride=2, padding=1)
         self.deconv2 = nn.ConvTranspose2d(512, 256, kernel_size=4, stride=2, padding=1)
@@ -43,19 +50,19 @@ class FeaturesToImage(nn.Module):
         self.deconv6 = nn.ConvTranspose2d(32, 16, kernel_size=4, stride=2, padding=1)
         self.deconv7 = nn.ConvTranspose2d(16, 1, kernel_size=4, stride=2, padding=1)
         
-        self.relu = nn.ReLU()
+        self.gelu = nn.GELU()
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
         # x is an array of indices of the training image, (batch_size)
         x = self.embedding(x) # (batch_size, num_features)
         x = x.view(x.size(0), x.size(1), 1, 1) # (batch_size, num_features, 1, 1)
-        x = self.relu(self.deconv1(x)) # (batch_size, 512, 2, 2)
-        x = self.relu(self.deconv2(x)) # (batch_size, 256, 4, 4)
-        x = self.relu(self.deconv3(x)) # (batch_size, 128, 8, 8)
-        x = self.relu(self.deconv4(x)) # (batch_size, 64, 16, 16)
-        x = self.relu(self.deconv5(x)) # (batch_size, 32, 32, 32)
-        x = self.relu(self.deconv6(x)) # (batch_size, 16, 64, 64)
+        x = self.bn1(self.gelu(self.deconv1(x))) # (batch_size, 512, 2, 2)
+        x = self.bn2(self.gelu(self.deconv2(x))) # (batch_size, 256, 4, 4)
+        x = self.bn3(self.gelu(self.deconv3(x))) # (batch_size, 128, 8, 8)
+        x = self.bn4(self.gelu(self.deconv4(x))) # (batch_size, 64, 16, 16)
+        x = self.bn5(self.gelu(self.deconv5(x))) # (batch_size, 32, 32, 32)
+        x = self.bn6(self.gelu(self.deconv6(x))) # (batch_size, 16, 64, 64)
         x = self.sigmoid(self.deconv7(x)) # (batch_size, 1, 128, 128)
         x = x.view(x.size(0), 128, 128)
         return x
@@ -63,13 +70,14 @@ class FeaturesToImage(nn.Module):
     def get_image_from_features(self, x): # skip the embedding step, manually generating from features
         # x is a tensor of features, shape (batch_size, num_features)
         x = x.view(x.size(0), x.size(1), 1, 1) # (batch_size, num_features, 1, 1)
-        x = self.relu(self.deconv1(x)) # (batch_size, 512, 2, 2)
-        x = self.relu(self.deconv2(x)) # (batch_size, 256, 4, 4)
-        x = self.relu(self.deconv3(x)) # (batch_size, 128, 8, 8)
-        x = self.relu(self.deconv4(x)) # (batch_size, 64, 16, 16)
-        x = self.relu(self.deconv5(x)) # (batch_size, 32, 32, 32)
-        x = self.relu(self.deconv6(x)) # (batch_size, 16, 64, 64)
+        x = self.bn1(self.gelu(self.deconv1(x))) # (batch_size, 512, 2, 2)
+        x = self.bn2(self.gelu(self.deconv2(x))) # (batch_size, 256, 4, 4)
+        x = self.bn3(self.gelu(self.deconv3(x))) # (batch_size, 128, 8, 8)
+        x = self.bn4(self.gelu(self.deconv4(x))) # (batch_size, 64, 16, 16)
+        x = self.bn5(self.gelu(self.deconv5(x))) # (batch_size, 32, 32, 32)
+        x = self.bn6(self.gelu(self.deconv6(x))) # (batch_size, 16, 64, 64)
         x = self.sigmoid(self.deconv7(x)) # (batch_size, 1, 128, 128)
+        x = x.view(x.size(0), 128, 128)
         return x
 
 # modified MSE loss, to get the model to pay more attention to non-zero pixels
@@ -115,7 +123,7 @@ for epoch in range(num_epochs):
     epoch_time = epoch_end_time - epoch_start_time
     if epoch % epoch_print_interval == 0 or epoch == num_epochs - 1:
         print(f"Epoch {epoch} of {num_epochs}, Loss: {loss.item()}, Time: {epoch_time:.2f} seconds")
-        print(f"    Saving truth and generated image side by side for three events...")
+        print(f"    Saving truth and generated images")
         fig, axs = plt.subplots(2, 5, figsize=(10, 5))
         for event_i in range(5):
             axs[0, event_i].imshow(y_train_batch.detach().cpu().numpy()[event_i,:,:], cmap="jet", vmin=0, vmax=1)
